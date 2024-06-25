77b484a25e317f6101ef47b2fa4b70813484bba0
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-06-25 10:53:15 +0530

    Initial Commit

---
+++ Spotify-clone/Spotify_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_cloneApp.swift
+//  Spotify-clone
+//
+//  Created by Piyush saini on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
